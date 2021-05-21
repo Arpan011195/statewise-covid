f8360bcfa7c89231baa6f9fcaf6c0df81bbf0895
Arpan Podder <arpantig@gmail.com>
2021-05-21 08:43:35 +0530

    first comit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Statewise from './components/statewise/statewise'
+function App() {
+  return (
+    <div>
+      <Statewise />
+    </div>
+  );
+}
+
+export default App;
